c82c6f8e699054bdc2074194ab0dd1055bf6d200
cabbaboi <71913806+cabbaboi@users.noreply.github.com>
2021-05-06 11:02:26 +0530

    Update news.js

--- news.js
+++ news.js
@@ -13,35 +13,86 @@ let apiKey = '7697528e901373d545763d39dc2b5146';
 // let apiKey = 'cd7bab009c4dba422b7693d995e537d4';
 // ${apiKey}
 
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', `https://api.currentsapi.services/v1/search?keywords=anime&language=en&country=jp&apiKey=RzUh0yVak4KtwfzekN40A-RikhpaFZUfvJ67_vu5pWF6vj28`, true);
+
+// // What to do when response is ready
+// xhr.onload = function () {
+//     if (this.status === 200) {
+//         let json = JSON.parse(this.responseText);
+//         let articles = json.news;
+//         console.log(articles);
+//         let newsHtml = "";
+//         articles.forEach(function(element, index) {
+//             console.log(element, index)
+//             let news = `
+//                             <div class="example-2 card">
+//                             <div class="wrapper" style="background-image: url(${element["image"]});">
+//                             <div class="header">
+//                                 <div class="date">
+//                                 </div>
+//                                 <ul class="menu-content">
+//                                 <li><a><span>${element["published"]}</span></a></li>
+//                                 </ul>
+//                             </div>
+//                             <div class="data">
+//                                 <div class="content">
+//                                 <span class="author">${element["author"]}</span>
+//                                 <h1 class="title"><a>${element["title"]}</a></h1>
+//                                 <div class="backdrop"><p class="text">${element["description"]}
+//                                 </p></div>
+//                                 <a target="_blank" href="${element['url']}" class="button">Read more</a>
+//                                 </div>
+//                             </div>
+//                             </div>
+//                         </div>
+//                         `;
+//             newsHtml += news;
+//         });
+//         collumn.innerHTML = newsHtml;
+        
+//         collumn0.innerHTML =`<button id="button" class="mbutton" onclick="myFunction()">Load more...</button>`;
+
+
+
+
+//     }
+//     else {
+//         console.log("Some error occured")
+//     }
+// }
+
+// xhr.send()
+
 const xhr = new XMLHttpRequest();
-xhr.open('GET', `https://api.currentsapi.services/v1/search?keywords=anime&language=en&country=jp&apiKey=RzUh0yVak4KtwfzekN40A-RikhpaFZUfvJ67_vu5pWF6vj28`, true);
+xhr.open('GET', `https://gnewsapi.net/api/search?q=anime&language=en&country=uk&api_token=BGUsbzrxzRRxlewkRmKQ1rND9EW5O21DwiuQAI03rretcrX7zSv6eY8pWLmv`, true);
 
 // What to do when response is ready
 xhr.onload = function () {
     if (this.status === 200) {
         let json = JSON.parse(this.responseText);
-        let articles = json.news;
+        let articles = json.articles;
         console.log(articles);
         let newsHtml = "";
         articles.forEach(function(element, index) {
             console.log(element, index)
             let news = `
                             <div class="example-2 card">
-                            <div class="wrapper" style="background-image: url(${element["image"]});">
+                            <div class="wrapper" style="background-image: url(${element["image_url"]});">
                             <div class="header">
                                 <div class="date">
                                 </div>
                                 <ul class="menu-content">
-                                <li><a><span>${element["published"]}</span></a></li>
+                                <li><a><span>${element["published_datetime"]}</span></a></li>
                                 </ul>
                             </div>
                             <div class="data">
                                 <div class="content">
-                                <span class="author">${element["author"]}</span>
+                                <span class="author">${element["source_name"]}</span>
                                 <h1 class="title"><a>${element["title"]}</a></h1>
                                 <div class="backdrop"><p class="text">${element["description"]}
                                 </p></div>
-                                <a target="_blank" href="${element['url']}" class="button">Read more</a>
+                                <a target="_blank" href="${element['article_url']}" class="button">Read more</a>
                                 </div>
                             </div>
                             </div>
